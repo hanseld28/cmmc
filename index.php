4774eb748596a48957003cf6961e797aee7f08e4
Hansel <hansel.donizete@gmail.com>
2020-01-20 13:27:57 -0300

    Estrutura inicial

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8"/>
+	<meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport">
+	<title>CMMC - Vereadores e Projeto de Lei Ordinária</title>
+
+    <link href="css/style.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" integrity="sha384-nn4HPE8lTHyVtfCBi5yW9d20FjT8BJwUXyWZT9InLYax14RDjBj46LmSztkmNP9w" crossorigin="anonymous">
+    <script async src="js/tabbis.js"></script>
+    <script async src="js/script.js"></script>
+</head>
+
+
+<body>
+    <header>
+        <img alt="logo" src="http://www.cmmc.com.br/logo.jpg">   
+    </header>
+    
+    <div data-tabs id="abas">
+        <div>Projetos de Lei Ordinária</div>
+        <div>Vereadores</div>
+    </div>
+
+    <div data-panes>
+		<div>Projetos... </div>
+		<div>Vereadores | Quantidade de projetos por status...</div>
+	</div>
+
+    
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,10 @@
+var tabs = tabbis.init({
+    tabGroup: '[data-tabs]',
+    paneGroup: '[data-panes]',
+    tabActive: 'active',
+    paneActive: 'active',
+    callback: function(tab, pane) {
+        console.log(tab);
+        console.log(pane);
+    }
+});
